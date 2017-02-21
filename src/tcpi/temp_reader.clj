b710c6ebf9ebe16dc63dfa3c720d718a50aba29c
Matti Ahinko <matti.ahinko@gofore.com>
2017-02-21 21:59:37 +0200

    Add class for parsing temperature

---
+++ src/tcpi/temp_reader.clj
@@ -0,0 +1,22 @@
+(ns tcpi.temp-reader
+  (:require [clojure.string :as str])
+  (:gen-class))
+
+(defn is-valid?
+  [raw]
+  (and
+    (str/includes? raw "YES")
+    (str/includes? raw "t=")))
+
+(defn parse-temperature
+  [raw]
+  (str/trim
+    (subs
+      raw
+      (+ (str/index-of raw "t=") 2))))
+
+(defn parse-double-temperature
+  [raw]
+  (/
+    (bigdec (parse raw))
+    1000.0))
