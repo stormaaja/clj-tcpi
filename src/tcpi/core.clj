febfb8aff77638b2a325cae8a1a62281115da311
Matti Ahinko <matti.ahinko@gofore.com>
2017-02-21 22:03:17 +0200

    Reorder functions

--- src/tcpi/core.clj
+++ src/tcpi/core.clj
@@ -2,12 +2,12 @@
   (:require [tcpi.temp-reader :as temp])
   (:gen-class))
 
-(defn -main
-  [& args]
-  (println
-    (read-temperature (first args))))
-
 (defn read-temperature
   [filepath]
   (temp/parse-double-temperature
       (slurp filepath)))
+
+(defn -main
+  [& args]
+  (println
+    (read-temperature (first args))))
