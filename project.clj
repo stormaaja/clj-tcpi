1e44301d9cb637a31be670e5620dbef30b3aaf1a
Matti Ahinko <matti.ahinko@gofore.com>
2017-02-21 21:34:41 +0200

    Add test profile

--- project.clj
+++ project.clj
@@ -3,7 +3,14 @@
   :url "https://github.com/stormaaja/clj-tcpi"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot tcpi.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {
+    :uberjar {:aot :all}
+    :test {
+      :dependencies [
+        [org.clojure/algo.generic "0.1.2"]]
+    }
+  })
