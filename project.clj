2a7c90bef8c00c89b2e65d87dc05c81deb1db862
Matti Ahinko <matti.ahinko@gofore.com>
2017-02-21 18:12:48 +0200

    Add base project file

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject tcpi "0.1.0-SNAPSHOT"
+  :description "Temperature controller for Raspberry Pi powered by Clojure"
+  :url "https://github.com/stormaaja/clj-tcpi"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot tcpi.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
