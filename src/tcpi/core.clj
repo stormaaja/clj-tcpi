560630552f007cf3f82b2f344db73258ed13b2b7
Matti Ahinko <matti.ahinko@gofore.com>
2017-03-05 16:45:23 +0200

    Add using thermostat

--- src/tcpi/core.clj
+++ src/tcpi/core.clj
@@ -1,16 +1,32 @@
 (ns tcpi.core
-  (:require [tcpi.temp-reader :as temp])
+  (:require [tcpi.thermostat :as thermo])
   (:gen-class))
 
-(defn read-temperature
-  [filepath]
-  (def content (slurp filepath))
-  (if (temp/is-valid? content)
-    (temp/parse-double-temperature
-      content)
-    nil))
+(defn heating-to-string
+  [heating]
+  (if heating
+    "Heating"
+    "Idle"))
+
+(defn print-state
+  [temperature state]
+  (printf "%f/%f %s %d          \r"
+    (:current temperature)
+    (:target temperature)
+    (heating-to-string (:heating state))
+    (:time state))
+  (flush))
+
+(defn third
+  [coll]
+  (nth coll 2))
 
 (defn -main
   [& args]
-  (println
-    (read-temperature (first args))))
+  (if (= (count args) 3)
+    (thermo/start-keep-heat
+      (first args)
+      (Double/parseDouble (second args))
+      (Integer/parseInt (third args))
+      print-state)
+    (println "Missing argumenst: sensor target pin")))
