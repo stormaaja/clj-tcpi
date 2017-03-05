b15a5d12a3e1452ed80dd0c067f7ddeb9e5d1bf1
Matti Ahinko <matti.ahinko@gofore.com>
2017-03-05 15:23:41 +0200

    Add http handler

---
+++ src/tcpi/handler.clj
@@ -0,0 +1,11 @@
+(ns tcpi.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+  (GET "/" [] "index")
+  (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
