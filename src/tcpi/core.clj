2fc3bf9d79dedca77a47b36d89edabae39b98b7c
Matti Ahinko <matti.ahinko@gofore.com>
2017-03-05 21:20:38 +0200

    Add web server

--- src/tcpi/core.clj
+++ src/tcpi/core.clj
@@ -1,5 +1,11 @@
 (ns tcpi.core
-  (:require [tcpi.thermostat :as thermo])
+  (:require [tcpi.thermostat :as thermo]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.logger :as logger]
+            [ring.middleware.reload :as reload]
+            [clojure.data.json :as json])
+  (:use org.httpkit.server)
   (:gen-class))
 
 (defn heating-to-string
@@ -21,7 +27,7 @@
   [coll]
   (nth coll 2))
 
-(defn -main
+(defn console-main
   [& args]
   (if (= (count args) 3)
     (thermo/start-keep-heat
@@ -30,3 +36,50 @@
       (Integer/parseInt (third args))
       print-state)
     (println "Missing argumenst: sensor target pin")))
+
+(defonce channels (atom #{}))
+
+(defn broadcast [message]
+  (do
+    (doseq [channel @channels]
+      (send! channel message))))
+
+(defn on-channel-close
+  [channel status]
+  (swap! channels #(remove #{channel} %)))
+
+(defn on-channel-open
+  [channel]
+  (swap! channels conj channel))
+
+(defn on-channel-receive
+  [channel data])
+
+(defn ws-handler [req]
+  (with-channel req channel
+    (on-close channel (partial on-channel-close channel))
+    (if (websocket? channel)
+      (on-channel-open channel))
+    (on-receive channel (partial on-channel-receive channel))))
+
+(defroutes app-routes
+  (route/resources "/")
+  (GET "/ws" [] ws-handler)
+  (route/not-found "Not Found"))
+
+(defn -main
+  [& args]
+  (org.apache.log4j.BasicConfigurator/configure)
+  (println "Starting server")
+  (run-server
+    (logger/wrap-with-logger (reload/wrap-reload #'app-routes))
+    {:port 8080})
+  (println "Starting thermostat")
+  (thermo/start-keep-heat
+    "/tmp/sensor"
+    30.0
+    17
+    (fn [temperature state]
+      (broadcast (json/write-str
+        { :temperature temperature
+          :state state })))))
