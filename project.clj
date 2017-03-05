9fca5d533a04514ec1d5b61a57637aa2e36bb00f
Matti Ahinko <matti.ahinko@gofore.com>
2017-03-05 21:20:52 +0200

    Add dependencies

--- project.clj
+++ project.clj
@@ -8,7 +8,10 @@
     [org.clojure/clojure "1.8.0"]
     [clj-raspberry-gpio "0.1.2"]
     [compojure "1.5.1"]
-    [ring/ring-defaults "0.2.1"]]
+    [ring "1.5.1"]
+    [ring.middleware.logger "0.5.0"]
+    [http-kit "2.2.0"]
+    [org.clojure/data.json "0.2.6"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler tcpi.handler/app}
   :main ^:skip-aot tcpi.core
