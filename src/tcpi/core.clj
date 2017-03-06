6d4a6b8408c28ac7d35d3a9617391447b22b0318
Matti Ahinko <matti.ahinko@gofore.com>
2017-03-06 18:14:55 +0200

    Remove unused functions

--- src/tcpi/core.clj
+++ src/tcpi/core.clj
@@ -24,20 +24,6 @@
     (:time state))
   (flush))
 
-(defn third
-  [coll]
-  (nth coll 2))
-
-(defn console-main
-  [& args]
-  (if (= (count args) 3)
-    (thermo/start-keep-heat
-      (first args)
-      (Double/parseDouble (second args))
-      (Integer/parseInt (third args))
-      print-state)
-    (println "Missing argumenst: sensor target pin")))
-
 (defonce channels (atom #{}))
 
 (defn broadcast [message]
